0b812e92148021360dff37b3c429811c1cf0bd6c
Oscar Bernardini <bernardini687@gmail.com>
2019-08-20 11:38:59 +0200

    Write unit tests for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user with valid email should pass' do
+    user = User.new email: 'user@user.com', password_digest: 'raspberry'
+
+    assert user.valid?
+  end
+
+  test 'user with unvalid email should not pass' do
+    user = User.new email: 'user', password_digest: 'raspberry'
+
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should not pass' do
+    other_user = users :one
+
+    user = User.new email: other_user.email, password_digest: 'raspberry'
+
+    assert_not user.valid?
+  end
 end
