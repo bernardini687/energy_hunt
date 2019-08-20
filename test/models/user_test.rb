93cff36faaf8f5e7a809a6ad359071e7733dccf5
Oscar Bernardini <bernardini687@gmail.com>
2019-08-20 11:44:18 +0200

    Add tests for empty inputs

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,13 +2,25 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'user with valid email should pass' do
-    user = User.new email: 'user@user.com', password_digest: 'raspberry'
+    user = User.new email: 'user@user.com', password_digest: :rapsberry
 
     assert user.valid?
   end
 
+  test 'user with no email should not pass' do
+    user = User.new password_digest: :raspberry
+
+    assert_not user.valid?
+  end
+
+  test 'user with no password should not pass' do
+    user = User.new email: 'user@user.com'
+
+    assert_not user.valid?
+  end
+
   test 'user with unvalid email should not pass' do
-    user = User.new email: 'user', password_digest: 'raspberry'
+    user = User.new email: 'user', password_digest: :rapsberry
 
     assert_not user.valid?
   end
@@ -16,7 +28,7 @@ class UserTest < ActiveSupport::TestCase
   test 'user with taken email should not pass' do
     other_user = users :one
 
-    user = User.new email: other_user.email, password_digest: 'raspberry'
+    user = User.new email: other_user.email, password_digest: :rapsberry
 
     assert_not user.valid?
   end
